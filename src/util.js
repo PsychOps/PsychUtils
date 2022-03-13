ffd5bf91fb0341e0405fac9fa16b73bbe5eb0d68
joshua <zoniqhedgehog@gmail.com>
2022-03-13 20:45:19 +0100

    Add minimal required functionality

---
+++ src/util.js
@@ -0,0 +1,12 @@
+const config = require('../config.json');
+
+const util = {};
+
+util.color = {
+    blue: 0xadd8e6,
+    red: 0xf04747,
+    green: 0x90ee90,
+    orange: 0xfaa61a,
+};
+
+module.exports = util;
